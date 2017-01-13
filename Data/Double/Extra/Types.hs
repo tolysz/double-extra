0845013b5499154ebd991a7f3b5d77c8a9679b15
Marcin Tolysz <tolysz@gmail.com>
2017-01-13 11:20:49 +0000

    Add roles

--- Data/Double/Extra/Types.hs
+++ Data/Double/Extra/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RoleAnnotations #-}
 module Data.Double.Extra.Types
  ( DoublePrecision(..)
  , DoubleExponential(..)
@@ -13,12 +14,15 @@ import GHC.Generics
 
 newtype DoublePrecision (i :: Nat) = DoublePrecision Double
   deriving (Eq,Enum,Ord,Show,Read,Generic,NFData,Num,Fractional,Floating,Real,RealFrac)
+type role DoublePrecision phantom
 
 newtype DoubleExponential (i :: Nat) = DoubleExponential Double
   deriving (Eq,Enum,Ord,Show,Read,Generic,NFData,Num,Fractional,Floating,Real,RealFrac)
+type role DoubleExponential phantom
 
 newtype DoubleFixed (i :: Nat) = DoubleFixed Double
   deriving (Eq,Enum,Ord,Show,Read,Generic,NFData,Num,Fractional,Floating,Real,RealFrac)
+type role DoubleFixed phantom
 
 newtype DoubleShortest = DoubleShortest Double
   deriving (Eq,Enum,Ord,Show,Read,Generic,NFData,Num,Fractional,Floating,Real,RealFrac)
