339ccdc1691e8a5f9d09603f28f13da2eacf250e
Marcin Tolysz <tolysz@gmail.com>
2017-05-02 12:06:40 +0100

    Add reading instances for cassava

--- Data/Double/Extra/Cassava.hs
+++ Data/Double/Extra/Cassava.hs
@@ -4,6 +4,8 @@ import Data.Double.Extra.Types
 import Data.Double.Conversion.ByteString
 import qualified Data.Csv as CSV
 
+-- Writing 
+
 instance KnownNat n => CSV.ToField (DoubleFixed n) where
   toField x = toFixed (fromIntegral (natVal x)) $ coerce x
 
@@ -15,3 +17,17 @@ instance KnownNat n => CSV.ToField (DoubleExponential n) where
 
 instance CSV.ToField DoubleShortest where
   toField = coerce toShortest
+
+-- Reading
+
+instance KnownNat n => CSV.FromField (DoubleFixed n) where
+  parseField = coerce (CSV.parseField :: CSV.Field -> CSV.Parser Double)
+
+instance KnownNat n => CSV.FromField (DoublePrecision n) where
+  parseField = coerce (CSV.parseField :: CSV.Field -> CSV.Parser Double)
+
+instance KnownNat n => CSV.FromField (DoubleExponential n) where
+  parseField = coerce (CSV.parseField :: CSV.Field -> CSV.Parser Double)
+
+instance CSV.FromField DoubleShortest where
+  parseField = coerce (CSV.parseField :: CSV.Field -> CSV.Parser Double)
